f2bf8a0c611e9245bafe802d0b0aa8e8577c0886
Rodolfo Ghiggi <rodolfo@maxiprod.com.br>
2021-03-27 14:48:46 -0300

    Aula 1: Primeiros passos

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    println("Bem vindo ao Bytebank!")
+    val titular = "Rodolfo Ghiggi"
+    val numeroConta = 1000
+    var saldo = 0.0
+    saldo = 100 + 2.0
+    saldo += 200
+
+    println("titular: $titular")
+    println("número da conta: $numeroConta")
+    println("saldo: $saldo")
+}
