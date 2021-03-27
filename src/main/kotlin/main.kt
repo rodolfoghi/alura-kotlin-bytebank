19f8bd2da6afce3df78d21aaf713af13ac446059
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2021-03-27 15:24:00 -0300

    Utilizando estrutura de repetição

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,15 +1,58 @@
 fun main() {
     println("Bem vindo ao Bytebank!")
-    val titular = "Rodolfo Ghiggi"
-    val numeroConta = 1000
-    var saldo = 100 + 2.0
-    saldo += 200
 
-    println("titular: $titular")
-    println("número da conta: $numeroConta")
-    println("saldo: $saldo")
+    testaEstruturasRepeticao()
+}
 
-//    testaCondicoes(saldo)
+fun testaEstruturasRepeticao() {
+    testaFor()
+    testaForDownTo()
+    testaWhile()
+}
+
+private fun testaWhile() {
+    var i = 0
+    while (i < 5) {
+        println(i)
+        i++
+    }
+}
+
+private fun testaForDownTo() {
+    for (i in 5 downTo 1 step 2) {
+
+        if (i == 3) {
+            break
+        }
+
+        if (i == 1) {
+            continue
+        }
+
+        val titular = "Rodolfo $i"
+        val numeroConta = 1000 + i
+        val saldo = i + 10.0
+
+        println("titular: $titular")
+        println("número da conta: $numeroConta")
+        println("saldo: $saldo")
+        testaCondicoes(saldo)
+        println()
+    }
+}
+
+private fun testaFor() {
+    for (i in 1..5) {
+        val titular = "Rodolfo $i"
+        val numeroConta = 1000 + i
+        val saldo = i + 10.0
+
+        println("titular: $titular")
+        println("número da conta: $numeroConta")
+        println("saldo: $saldo")
+        testaCondicoes(saldo)
+        println()
+    }
 }
 
 fun testaCondicoes(saldo: Double) {
